1ff2f7c004067843ca9c3b36d9b62f5741867ff0
Glitch (rapid-bejewled-caption) <none>
2023-02-24 20:44:24 +0000

    🧊🎺 Updated with Glitch

---
+++ src/pages/end.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+/* ADD IMPORTS FROM TODO ON THE NEXT LINE */
+
+
+/**
+* The About function defines the component that makes up the About page
+* This component is attached to the /about path in router.jsx
+*/
+
+export default function About() {
+  /* DECLARE STYLE AND TRIGGER FOR WIGGLE EFFECT FROM TODO ON NEXT LINE */
+  
+  return (
+    <div className="page">
+      <h1>you're at the end!</h1>
+    </div>
+  );
+}
+
